65cc6f73cceb0cc473c89eccfaf6db46cb1f53fc
mites1997 <arnabfeb15@gmail.com>
2021-08-13 09:25:50 +0530

    add various structural and component directives

--- hello/src/app/server/server.component.ts
+++ hello/src/app/server/server.component.ts
@@ -1,7 +1,13 @@
 import {Component} from '@angular/core';
 @Component({
     selector :'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles:[`
+    .online{
+        color:white;
+    }
+    `
+]
 }
 
 )
@@ -9,9 +15,18 @@ export class ServerComponent{
 
 serverId:number=10;
 serverStatus:string='offline';
+constructor(){
+this.serverStatus=Math.random()>0.5?"online":"offline";
+
+}
 getServerStatus()
 {
     return this.serverStatus;
+}
+getColor()
+{
+
+    return this.serverStatus==='online'?'green':'red';
 }
 
 }
--- hello/src/app/servers/servers.component.ts
+++ hello/src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus='No server was created';
   serverName='TestServer';
   serverCreated='false';
+  servers=['Testserver','Testserver 2']
 
   constructor() { 
 
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   onCreateServer()
   {
     this.serverCreated='true';
+    this.servers.push(this.serverName);
 this.serverCreationStatus='server was created';
 
   }
